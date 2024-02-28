49c6664f8b256f117acb715680dc1074927bd337
Inger-Margrethe <imhp@hotmail.dk>
2024-02-28 14:52:06 +0100

    pop up basics done

--- maaltids.js
+++ maaltids.js
@@ -1,5 +1,7 @@
 // THIS IS JAVASCRIPT FOR THE SUBPAGE MAALTIDSKASSER.HTML
 
+/*---Icon toggle functionality---*/
+
 // Get all the icons
 let iconImgs = document.querySelectorAll(".icon_imgs");
 
@@ -24,4 +26,50 @@ iconImgs.forEach(function(img) {
     img.addEventListener("click", function() {
         toggleImage(img);
     });
+});
+
+/*---POPUP---*/
+
+const openReste = document.getElementById("open_reste");
+const popUpReste = document.getElementById("pop_up_reste");
+
+openReste.addEventListener("click", () => {
+    popUpReste.showModal()
+})
+
+/*---counters---*/
+const plusPs = document.getElementById("plus_ps");
+const numPs = document.getElementById("ps_num");
+const minusPs = document.getElementById("minus_ps");
+
+let ps = 1;
+
+plusPs.addEventListener("click", ()=>{
+    ps++;
+    numPs.innerText = ps;
+});
+
+minusPs.addEventListener("click", ()=>{
+    if(ps > 1){
+        ps--;
+        numPs.innerText = ps;
+    }
+});
+
+const plusDg = document.getElementById("plus_dg");
+const numDg = document.getElementById("dg_num");
+const minusDg = document.getElementById("minus_dg");
+
+let dg = 3;
+
+plusDg.addEventListener("click", ()=>{
+    dg++;
+    numDg.innerText = dg;
+});
+
+minusDg.addEventListener("click", ()=>{
+    if(dg > 3){
+        dg--;
+        numDg.innerText = dg;
+    }
 });
